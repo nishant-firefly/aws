e5348a5d78c8cf90e23939fc95a4177d165d8761
nishant-firefly <nishu.saxena@gmail.com>
2024-07-13 22:53:06 -0400

    Added TODO: to enhance the check service, and for every service use Running Status to be returnmed with proper exception handling and not to returen True , False

--- up_system/chek_system.py
+++ up_system/chek_system.py
@@ -15,9 +15,25 @@ if __name__=="__main__":
             return RunningStatus(False, e, message=CheckSystem.EXC_MSG_DEFENSIVE_CODE_FOR_SERVICE.format(service_name))
 
     def check_docker() -> RunningStatus:
+        ## TODO : This is simple code, Exceptional hanling needed, Refer commit : 
+        ##**1 e.g in https://github.com/nishant-firefly/aws/compare/861a107bd738bd428bb98a984d845701db890013...8d6d74459c180cc1961690318227113f627dc328
+        ############ see the function check_docker 
+        """TODO continues :  Before it was written like folowing and due to above check_Service exception handling is causing not handle exception properly
+        def check_docker():
+            try:
+                client = docker.from_env()
+                client.ping()
+                return RunningStatus(True)
+            except DockerException as e:
+                return RunningStatus(False, e)
+            except Exception as e:
+                return RunningStatus(False, e, message="Defensive Coding, caught in generic exception")
+        
+        """
         return check_service("Docker",lambda: docker.from_env().ping())
 
     def check_localstack() -> RunningStatus:
+        # TODO : Localstack is just returning True False, Exception handling needed e.g. above ##**1
         return check_service("Localstack", LocalstackManager().check_localstack)
         ## Err of s3 not exist-- lambda: requests.get("http://localhost:4566/health").raise_for_status())
 
