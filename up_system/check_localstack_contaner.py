83dfec739b3a8a604bfee086422b612a7b13ba67
nishant-firefly <nishu.saxena@gmail.com>
2024-07-13 23:14:54 -0400

    All TODOs done code organised properly so can keep adding the services for testing

--- up_system/check_localstack_contaner.py
+++ up_system/check_localstack_contaner.py
@@ -1,9 +1,13 @@
-import os
 import yaml
 import docker
 from dotenv import load_dotenv
 from utils import pdm
 from enum import Enum
+from utils import RunningStatus
+from enum import Enum
+import docker
+from docker.errors import DockerException
+
 
 # Load environment variables from .env file
 load_dotenv('.env')
@@ -24,28 +28,43 @@ class LocalstackManager:
         with open(self.compose_file, 'r') as file:
             return yaml.safe_load(file)
 
-    def check_container_status(self, container_name):
+    def check_container_status(self, container_name) -> RunningStatus:
         """Check the status of a Docker container"""
         try:
             container = self.client.containers.get(container_name)
             if container.status == 'running':
                 pdm(LocalstackMessages.CONTAINER_RUNNING.value.format(container_name=container_name))
-                return True
+                return RunningStatus(True)
             else:
-                pdm(LocalstackMessages.CONTAINER_NOT_RUNNING.value.format(container_name=container_name, status=container.status))
-                return False
-        except docker.errors.NotFound:
+                msg=LocalstackMessages.CONTAINER_NOT_RUNNING.value.format(container_name=container_name, status=container.status)
+                pdm(msg)
+                return RunningStatus(True, message=msg)
+        except docker.errors.NotFound as e:
             pdm(LocalstackMessages.CONTAINER_NOT_FOUND.value.format(container_name=container_name))
-            return False
+            return RunningStatus(False,e)
         except docker.errors.APIError as e:
             pdm(LocalstackMessages.API_ERROR.value.format(error=str(e)))
-            return False
+            return RunningStatus(False,e)
+        
+    def check_docker(self) -> RunningStatus:
+        ## TODO : This is simple code, Exceptional hanling needed, Refer commit : 
+        ##**1 e.g in https://github.com/nishant-firefly/aws/compare/861a107bd738bd428bb98a984d845701db890013...8d6d74459c180cc1961690318227113f627dc328
+        ############ see the function check_docker 
+        try:
+            client = docker.from_env()
+            client.ping()
+            return RunningStatus(True)
+        except DockerException as e:
+            return RunningStatus(False, e)
+        except Exception as e:
+            return RunningStatus(False, e, message="Defensive Coding, caught in generic exception")
 
-    def check_localstack(self):
+    def check_localstack(self) -> RunningStatus:
         """Read Docker Compose configuration and check container status"""
+        # TODO: Handle following 2 lines if issue
         compose_config = self.read_docker_compose()
         container_name = compose_config['services']['localstack']['container_name']
-        self.check_container_status(container_name)
+        return self.check_container_status(container_name)
 
 if __name__ == "__main__":
     LocalstackManager().check_localstack()
--- up_system/chek_system.py
+++ up_system/chek_system.py
@@ -1,45 +1,10 @@
-from utils import CheckSystem, RunningStatus
-from enum import Enum
-import docker
 from check_localstack_contaner import LocalstackManager
-
+from utils import CheckSystem
 
 if __name__=="__main__":
-
-
-    def check_service(service_name: str, check_func) -> RunningStatus:
-        try:
-            check_func()
-            return RunningStatus(True)
-        except Exception as e:
-            return RunningStatus(False, e, message=CheckSystem.EXC_MSG_DEFENSIVE_CODE_FOR_SERVICE.format(service_name))
-
-    def check_docker() -> RunningStatus:
-        ## TODO : This is simple code, Exceptional hanling needed, Refer commit : 
-        ##**1 e.g in https://github.com/nishant-firefly/aws/compare/861a107bd738bd428bb98a984d845701db890013...8d6d74459c180cc1961690318227113f627dc328
-        ############ see the function check_docker 
-        """TODO continues :  Before it was written like folowing and due to above check_Service exception handling is causing not handle exception properly
-        def check_docker():
-            try:
-                client = docker.from_env()
-                client.ping()
-                return RunningStatus(True)
-            except DockerException as e:
-                return RunningStatus(False, e)
-            except Exception as e:
-                return RunningStatus(False, e, message="Defensive Coding, caught in generic exception")
-        
-        """
-        return check_service("Docker",lambda: docker.from_env().ping())
-
-    def check_localstack() -> RunningStatus:
-        # TODO : Localstack is just returning True False, Exception handling needed e.g. above ##**1
-        return check_service("Localstack", LocalstackManager().check_localstack)
-        ## Err of s3 not exist-- lambda: requests.get("http://localhost:4566/health").raise_for_status())
-
     status = CheckSystem({
-                "Docker": check_docker,
-                "Localstack":check_localstack
+                "Docker": LocalstackManager().check_docker,
+                "Localstack":LocalstackManager().check_localstack
                 # Add more services here
             }).check_system()
     print(status)
