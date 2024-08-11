36c6eb7298761901ef4eda8ee9b6b3096bbbe8e3
nishant-firefly <nishu.saxena@gmail.com>
2024-08-11 14:25:19 -0400

    Example Code moved to examples In main will do e2e

--- e2e/step/examples_aws/hello_step_functions.py
+++ e2e/step/examples_aws/hello_step_functions.py
@@ -3,6 +3,9 @@
 
 # snippet-start:[python.example_code.sfn.Hello]
 import boto3
+import os
+from dotenv import load_dotenv
+from step.examples_aws.hello_step_functions import hello_stepfunctions
 
 
 def hello_stepfunctions(stepfunctions_client):
@@ -22,5 +25,14 @@ def hello_stepfunctions(stepfunctions_client):
 
 
 if __name__ == "__main__":
-    hello_stepfunctions(boto3.client("stepfunctions"))
-# snippet-end:[python.example_code.sfn.Hello]
+
+    dotenv_path = os.path.join(os.path.dirname(__file__), '..', 'up_system', '.env')
+    load_dotenv(dotenv_path)
+    print(dotenv_path, os.path.exists(dotenv_path))
+
+    hello_stepfunctions(boto3.client("stepfunctions",
+        region_name=os.getenv("REGION_NAME"),
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        endpoint_url=os.getenv("LOCALSTACK_ENDPOINT")  # If using LocalStack
+    ))
